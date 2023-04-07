488bb93eaa81e4c8ed01a64581b06d9d52ebfd73
MartaSyvyk <shavaliuk.marta@gmail.com>
2023-04-07 15:20:49 +0300

    initial commit hw5

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,19 +6,22 @@ const {
   getCurrent,
   logout,
   updateSubscription,
+  updateAvatar,
 } = require("../../controller/users.js");
 const { validate } = require("../../middlewares/validation");
 const {
   registerJoiSchema,
   loginJoiSchema,
-  SubscriptionUpdJoiSchema,
-} = require("../../service/schemas/user");
+  subscriptionUpdJoiSchema,
+  } = require("../../service/schemas/user");
 const { auth } = require("../../middlewares/auth");
+const { upload } = require("../../middlewares/upload");
 
 router.post("/register", validate(registerJoiSchema), register);
 router.post("/login", validate(loginJoiSchema), login);
 router.get("/current", auth, getCurrent);
 router.get("/logout", auth, logout);
-router.patch("/", auth, validate(SubscriptionUpdJoiSchema), updateSubscription);
+router.patch("/", auth, validate(subscriptionUpdJoiSchema), updateSubscription);
+router.patch("/avatars", auth, upload.single("image"),  updateAvatar);
 
 module.exports = router;
